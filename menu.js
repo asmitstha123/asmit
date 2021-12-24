b9edb55f7a8467a456d8cdc27f14541af26e7302
Asmit Shrestha <asmith@vesuviois.com>
2021-12-24 09:12:23 +0545

    favicon

--- menu.js
+++ menu.js
@@ -1,4 +1,5 @@
 var elemnt = document.getElementById("menu");
+
            var main_elm = document.querySelector("main");
            var header_class = document.querySelector("header");
            var menu_close = document.querySelector(".menu-icon:not(.res)");
@@ -10,5 +11,7 @@ var elemnt = document.getElementById("menu");
                 document.getElementById("main").classList.toggle("display-main");
                 document.getElementById("icon").classList.toggle("close-menu");
             }
+
+           
 
       
